459b4aaf2c3601d7cf4be718807356adbee935e5
고영균 <gyg@goyeong-gyun-ui-MacBookPro.local>
2022-03-30 19:33:57 +0900

    hw03

--- hw03/myprog.sh
+++ hw03/myprog.sh
@@ -2,11 +2,13 @@
 
 if [ ! -d ./temp ]; then
 mkdir temp
+echo "create dir"
 fi
 
 cp ./num1.txt ./temp
 cp ./num2.txt ./temp
 cp ./cal.sh ./temp
+echo "copy files"
 
 echo "Select : add 1,  sub 2,  div 3,  mul 4"
 
